31fb01af608e505a56b1903c24dfb3e8172cd2f1
Onizukachi <dark_sao@mail.ru>
2023-11-12 13:16:53 +0300

    add redis and docker compose

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,9 +14,14 @@ module HikaruBlog
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     config.active_job.queue_adapter = :sidekiq
+
+    config.cache_store = :redis_store, 'redis://localhost:7963/0/cache', { expires_in: 90.minutes }
+
+    # To keep using the current cache store, you can turn off cache versioning entirely:
+    config.active_record.cache_versioning = false
 
     # Configuration for the application, engines, and railties goes here.
     #
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_11_07_112227) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text "body"
-    t.integer "question_id", null: false
+    t.bigint "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
