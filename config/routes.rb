aecf10da3b5cef164bfdc654d2305b7bdd6eedb3
Onizukachi <dark_sao@mail.ru>
2023-11-13 11:58:03 +0300

    update  tailwind config and add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
+
+  resources :users, only: %i[new create]
+
   resources :questions do
     resources :answers, except: %i[new show]
   end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
@@ -7,10 +9,18 @@ module.exports = {
     './node_modules/flowbite/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
-    require('flowbite/plugin')
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+    require('flowbite/plugin'),
   ],
   safelist: [
     'text-green-800',
