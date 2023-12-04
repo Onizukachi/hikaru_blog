df406abcd4b56e990fc78473e57a098ab9bbd02c
Onizukachi <dark_sao@mail.ru>
2023-12-04 09:39:19 +0300

    add turbo for answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -34,8 +34,18 @@ class AnswersController < ApplicationController
 
   def update
     if @answer.update answer_update_params
-      flash[:success] = t '.success'
-      redirect_to question_path(@question, anchor: helpers.dom_id(@answer))
+      respond_to do |format|
+        format.html do
+          flash[:success] = t '.success'
+          redirect_to question_path(@question, anchor: helpers.dom_id(@answer))
+        end
+
+        format.turbo_stream do
+          flash.now[:success] = t '.success'
+          @answer = @answer.decorate
+        end
+      end
+
     else
       render :edit
     end
